9bbfea66ce478e4efd2efdbcdcf3e45f384711b7
afeng7882999 <afeng7882999@163.com>
2022-01-11 14:34:07 +0800

    refactor(system/dictItem): change name of css classes, and change script to 'setup script

--- src/views/modules/system/dict-item/edit.vue
+++ src/views/modules/system/dict-item/edit.vue
@@ -1,25 +1,37 @@
 <template>
-  <el-dialog v-model="visible" :close-on-click-modal="false" :title="isCreate ? '新增' : '修改'" @closed="resetForm">
-    <el-form ref="form" :model="formData" :rules="formRule" label-width="100px" size="medium" @keyup.enter="submit()">
+  <el-dialog
+    v-model="state.visible"
+    :close-on-click-modal="false"
+    :title="state.isCreate ? '新增' : '修改'"
+    @closed="resetForm"
+  >
+    <el-form
+      ref="form"
+      :model="state.formData"
+      :rules="state.formRule"
+      label-width="100px"
+      size="medium"
+      @keyup.enter="submit()"
+    >
       <el-form-item label="字典ID" prop="dictId">
-        <el-input v-model="formData.dictId" disabled placeholder="请输入字典ID"></el-input>
+        <el-input v-model="state.formData.dictId" disabled placeholder="请输入字典ID"></el-input>
       </el-form-item>
       <el-form-item label="字典项键值" prop="itemKey">
-        <el-input v-model="formData.itemKey" placeholder="请输入字典项键值"></el-input>
+        <el-input v-model="state.formData.itemKey" placeholder="请输入字典项键值"></el-input>
       </el-form-item>
       <el-form-item label="字典项值" prop="itemValue">
-        <el-input v-model="formData.itemValue" placeholder="请输入字典项值"></el-input>
+        <el-input v-model="state.formData.itemValue" placeholder="请输入字典项值"></el-input>
       </el-form-item>
       <el-form-item label="备注信息" prop="remarks">
-        <el-input v-model="formData.remarks" placeholder="请输入备注信息"></el-input>
+        <el-input v-model="state.formData.remarks" placeholder="请输入备注信息"></el-input>
       </el-form-item>
       <el-form-item label="排序" prop="sort">
-        <el-input v-model="formData.sort" placeholder="请输入排序"></el-input>
+        <el-input v-model="state.formData.sort" placeholder="请输入排序"></el-input>
       </el-form-item>
     </el-form>
     <template #footer>
       <span class="fd-dialog-footer">
-        <el-button size="medium" @click="visible = false">取消</el-button>
+        <el-button size="medium" @click="state.visible = false">取消</el-button>
         <el-button size="medium" type="primary" @click="submit">确定</el-button>
       </span>
     </template>
@@ -27,49 +39,49 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, toRefs } from 'vue'
+export default {
+  name: 'SystemDictItemEdit'
+}
+</script>
+
+<script setup lang="ts">
 import useListEdit, { REFRESH_DATA_EVENT } from '@/components/crud/use-list-edit'
-import { dictItemFields, dictItemGetOne, dictItemPostOne, dictItemPutOne } from '@/api/system/dict-item.ts'
+import { dictItemFields, dictItemGetOne, dictItemPostOne, dictItemPutOne } from '@/api/system/dict-item'
 
-export default defineComponent({
-  name: 'SystemDictItemEdit',
-  emits: [REFRESH_DATA_EVENT],
-  setup(props, { emit }) {
-    const stateOption = {
-      idField: dictItemFields.idField,
-      getApi: dictItemGetOne,
-      postApi: dictItemPostOne,
-      putApi: dictItemPutOne,
-      resetFormData: {
-        id: '',
-        dictId: '',
-        itemKey: '',
-        itemValue: '',
-        remarks: '',
-        sort: 0
-      },
-      formRule: {
-        dictId: [{ required: true, message: '字典ID不能为空', trigger: 'blur' }],
-        itemKey: [{ required: true, message: '字典项键值不能为空', trigger: 'blur' }],
-        itemValue: [{ required: true, message: '字典项值不能为空', trigger: 'blur' }]
-      }
-    }
+const emit = defineEmits([REFRESH_DATA_EVENT])
 
-    const { mixRefs, mixState, mixMethods } = useListEdit(stateOption, emit)
-
-    // create/edit dialog
-    const openDictItemEdit = async (dictId: string, id: string) => {
-      mixState.formData.dictId = dictId
-      mixState.resetFormData.dictId = dictId
-      await mixMethods.open(id)
-    }
-
-    return {
-      ...mixRefs,
-      ...toRefs(mixState),
-      ...mixMethods,
-      openDictItemEdit
-    }
+const stateOption = {
+  idField: dictItemFields.idField,
+  getApi: dictItemGetOne,
+  postApi: dictItemPostOne,
+  putApi: dictItemPutOne,
+  resetFormData: {
+    id: '',
+    dictId: '',
+    itemKey: '',
+    itemValue: '',
+    remarks: '',
+    sort: 0
+  },
+  formRule: {
+    dictId: [{ required: true, message: '字典ID不能为空', trigger: 'blur' }],
+    itemKey: [{ required: true, message: '字典项键值不能为空', trigger: 'blur' }],
+    itemValue: [{ required: true, message: '字典项值不能为空', trigger: 'blur' }]
   }
+}
+
+const { mixRefs, mixState: state, mixMethods } = useListEdit(stateOption, emit)
+const { form } = mixRefs
+const { open, resetForm, submit } = mixMethods
+
+// create/edit dialog
+const openDictItemEdit = async (dictId: string, id: string) => {
+  state.formData.dictId = dictId
+  state.resetFormData.dictId = dictId
+  await open(id)
+}
+
+defineExpose({
+  openDictItemEdit
 })
 </script>
--- src/views/modules/system/dict-item/index.vue
+++ src/views/modules/system/dict-item/index.vue
@@ -2,19 +2,19 @@
   <div ref="moduleRoot" :style="pageMinHeight" class="system-page-dictItem fd-page">
     <fd-page-header v-show="showPageHeader"></fd-page-header>
     <div class="fd-page__form">
-      <el-form ref="queryForm" :inline="true" :model="query" size="medium" @keyup.enter="queryList()">
+      <el-form ref="queryForm" :inline="true" :model="state.query" size="medium" @keyup.enter="queryList()">
         <transition
           name="expand"
           @enter="expandEnter"
           @after-enter="expandAfterEnter"
           @before-leave="expandBeforeLeave"
         >
-          <div v-show="queryFormShow" class="fd-page__query">
+          <div v-show="state.queryFormShow" class="fd-page__query">
             <el-form-item label="字典项键值" prop="itemKey">
-              <el-input v-model="query.itemKey" clearable placeholder="请输入字典项键值" />
+              <el-input v-model="state.query.itemKey" clearable placeholder="请输入字典项键值" />
             </el-form-item>
             <el-form-item label="字典项值" prop="itemValue">
-              <el-input v-model="query.itemValue" clearable placeholder="请输入字典项值" />
+              <el-input v-model="state.query.itemValue" clearable placeholder="请输入字典项值" />
             </el-form-item>
             <el-form-item>
               <el-button plain type="primary" @click="queryList">
@@ -37,7 +37,7 @@
         <el-button
           v-show="hasAuth('system:dictItem:delete')"
           v-waves
-          :disabled="selectedNodes.length <= 0"
+          :disabled="state.selectedNodes.length <= 0"
           plain
           size="medium"
           type="danger"
@@ -62,13 +62,13 @@
           </el-button>
           <el-divider class="action-divider" direction="vertical"></el-divider>
           <el-tooltip
-            :content="queryFormShow ? '隐藏查询表单' : '显示查询表单'"
+            :content="state.queryFormShow ? '隐藏查询表单' : '显示查询表单'"
             :show-after="500"
             effect="dark"
             placement="top"
           >
             <fd-icon-button
-              :class="queryFormShow ? 'expanded' : ''"
+              :class="state.queryFormShow ? 'expanded' : ''"
               class="action-toggle-btn"
               icon="double-down"
               @click="toggleQueryForm()"
@@ -78,7 +78,7 @@
       </div>
     </div>
     <div class="fd-page__table border">
-      <el-table v-loading="loading" :data="data" row-key="id" @selection-change="onSelectionChange">
+      <el-table v-loading="state.loading" :data="state.data" row-key="id" @selection-change="onSelectionChange">
         <el-table-column align="left" header-align="left" type="selection" width="40"></el-table-column>
         <el-table-column
           :show-overflow-tooltip="true"
@@ -139,11 +139,11 @@
       </el-table>
       <el-pagination
         :background="true"
-        :current-page="current"
-        :page-count="total"
-        :page-size="size"
+        :current-page="state.current"
+        :page-count="state.total"
+        :page-size="state.size"
         :page-sizes="[10, 20, 50, 100, 200]"
-        :total="count"
+        :total="state.count"
         layout="total, sizes, prev, pager, next, jumper"
         @current-change="pageChange"
         @size-change="sizeChange"
@@ -154,12 +154,18 @@
       <fd-contextmenu-item @click="exportDictItemData()">导出当前数据</fd-contextmenu-item>
       <fd-contextmenu-item @click="exportDictItemData('all')">导出所有数据</fd-contextmenu-item>
     </fd-contextmenu>
-    <edit v-if="editShow" ref="editDialog" @refresh-data-list="getList"></edit>
+    <edit v-if="state.editShow" ref="editDialog" @refresh-data-list="getList"></edit>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, nextTick, onBeforeMount, ref, toRefs } from 'vue'
+export default {
+  name: 'SystemDictItem'
+}
+</script>
+
+<script setup lang="ts">
+import { nextTick, onBeforeMount, ref } from 'vue'
 import useList from '@/components/crud/use-list'
 import { dictItemDel, dictItemExport, dictItemFields, dictItemList, dictItemQuery } from '@/api/system/dict-item.ts'
 import Edit from './edit.vue'
@@ -168,88 +174,75 @@ import { useRoute, useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 import { dictGetOne } from '@/api/system/dict'
 
-export default defineComponent({
-  name: 'SystemDictItem',
-  components: { Edit },
-  setup() {
-    const contextMenu = ref()
+const contextMenu = ref()
 
-    const stateOption = {
-      idField: dictItemFields.idField,
-      listApi: dictItemList,
-      delApi: dictItemDel,
-      exportApi: dictItemExport,
-      query: dictItemQuery,
+const stateOption = {
+  idField: dictItemFields.idField,
+  listApi: dictItemList,
+  delApi: dictItemDel,
+  exportApi: dictItemExport,
+  query: dictItemQuery,
 
-      dictName: ''
-    }
+  dictName: ''
+}
 
-    const route = useRoute()
-    const router = useRouter()
-    const store = useStore()
+const route = useRoute()
+const router = useRouter()
+const store = useStore()
 
-    const { mixRefs, mixState, mixComputed, mixMethods } = useList(stateOption)
+const { mixRefs, mixState: state, mixComputed, mixMethods } = useList(stateOption)
+const { queryForm, editDialog } = mixRefs
+const { pageMinHeight, showPageHeader } = mixComputed
+const { getList, pageChange, sizeChange, queryList, resetQuery, del, hasAuth, onSelectionChange, toggleQueryForm } =
+  mixMethods
 
-    onBeforeMount(async () => {
-      const { id } = route.params
-      if (id) {
-        mixState.query.dictId = id as string
-        try {
-          const data = await dictGetOne(id as string)
-          mixState.dictName = data.name
-        } catch (e) {
-          console.log(e)
-        }
-      }
-    })
+const { expandEnter, expandAfterEnter, expandBeforeLeave } = useExpandTransition()
 
-    // show create/edit dialog
-    const showDictItemEdit = (id?: string) => {
-      mixState.editShow = true
-      nextTick(() => {
-        ;(mixRefs.editDialog.value as any).openDictItemEdit(mixState.query.dictId as string, id)
-      })
-    }
-
-    // export data of dict items
-    const exportDictItemData = async (type?: string) => {
-      mixState.exportLoading = true
-      try {
-        if (type && type === 'all') {
-          await mixState.exportApi('字典条目数据', {})
-        } else {
-          await mixState.exportApi(`字典${mixState.dictName}数据`, { dictId: mixState.query.dictId })
-        }
-        mixState.exportLoading = false
-      } catch (e) {
-        console.log(e)
-        mixState.exportLoading = false
-      }
-    }
-
-    // open context menu
-    const openMenu = (e: MouseEvent) => {
-      ;(contextMenu.value as any).show(e)
-    }
-
-    // close page
-    const close = () => {
-      store.dispatch('view/delView', route)
-      router.push({ path: '/system/dict' })
-    }
-
-    return {
-      ...mixRefs,
-      ...toRefs(mixState),
-      ...mixComputed,
-      ...mixMethods,
-      ...useExpandTransition(),
-      contextMenu,
-      showDictItemEdit,
-      exportDictItemData,
-      openMenu,
-      close
+onBeforeMount(async () => {
+  const { id } = route.params
+  if (id) {
+    state.query.dictId = id as string
+    try {
+      const data = await dictGetOne(id as string)
+      state.dictName = data.name
+    } catch (e) {
+      console.log(e)
     }
   }
 })
+
+// show create/edit dialog
+const showDictItemEdit = (id?: string) => {
+  state.editShow = true
+  nextTick(() => {
+    ;(mixRefs.editDialog.value as any).openDictItemEdit(state.query.dictId as string, id)
+  })
+}
+
+// export data of dict items
+const exportDictItemData = async (type?: string) => {
+  state.exportLoading = true
+  try {
+    if (type && type === 'all') {
+      await state.exportApi('字典条目数据', {})
+    } else {
+      await state.exportApi(`字典${state.dictName}数据`, { dictId: state.query.dictId })
+    }
+    state.exportLoading = false
+  } catch (e) {
+    console.log(e)
+    state.exportLoading = false
+  }
+}
+
+// open context menu
+const openMenu = (e: MouseEvent) => {
+  ;(contextMenu.value as any).show(e)
+}
+
+// close page
+const close = () => {
+  store.dispatch('view/delView', route)
+  router.push({ path: '/system/dict' })
+}
 </script>
