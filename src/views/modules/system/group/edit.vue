051b5b22523968e894063a82fafcc56f2a810536
afeng7882999 <afeng7882999@163.com>
2022-01-20 15:18:42 +0800

    refactor(system/group): change name of css classes, and change script to 'setup script

--- src/views/modules/system/group/edit.vue
+++ src/views/modules/system/group/edit.vue
@@ -1,19 +1,29 @@
 <template>
-  <el-dialog v-model="visible" :close-on-click-modal="false" :title="isCreate ? '新增' : '修改'" @closed="hideDialog">
-    <el-form ref="form" :model="formData" :rules="formRule" label-width="100px" size="medium" @keyup.enter="submit">
-      <el-form-item label="用户组名称" prop="name">
-        <el-input v-model="formData.name" placeholder="请输入用户组名称"></el-input>
-      </el-form-item>
+  <el-dialog
+    v-model="state.visible"
+    :close-on-click-modal="false"
+    :title="state.isCreate ? '新增' : '修改'"
+    @closed="hideDialog"
+  >
+    <el-form ref="form" :model="state.formData" :rules="state.formRule" label-width="100px" size="medium">
       <el-form-item label="上级用户组" prop="parentId">
-        <fd-tree-select v-model="formData.parentId" :data-list="parentList" :tree-fields="{ labelField: 'name' }" style="width: 100%"></fd-tree-select>
+        <fd-tree-select
+          v-model="state.formData.parentId"
+          :data-list="state.parentList"
+          :tree-fields="{ labelField: 'name' }"
+          style="width: 100%"
+        ></fd-tree-select>
       </el-form-item>
       <el-form-item label="排序" prop="sort">
-        <el-input v-model="formData.sort" placeholder="请输入排序"></el-input>
+        <el-input v-model="state.formData.sort" placeholder="请输入排序"></el-input>
+      </el-form-item>
+      <el-form-item label="用户组名称" prop="name">
+        <el-input v-model="state.formData.name" placeholder="请输入用户组名称"></el-input>
       </el-form-item>
     </el-form>
     <template #footer>
       <span class="fd-dialog-footer">
-        <el-button size="medium" @click="visible = false">取消</el-button>
+        <el-button size="medium" @click="state.visible = false">取消</el-button>
         <el-button size="medium" type="primary" @click="submit">确定</el-button>
       </span>
     </template>
@@ -21,39 +31,41 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, toRefs } from 'vue'
+export default {
+  name: 'SystemGroupEdit'
+}
+</script>
+
+<script setup lang="ts">
 import useTreeEdit, { REFRESH_DATA_EVENT } from '@/components/crud/use-tree-edit'
-import { groupFields, groupGetOne, groupList, groupPostOne, groupPutOne, groupTreeFields } from '@/api/system/group'
+import { groupFields, groupTreeFields, groupList, groupGetOne, groupPostOne, groupPutOne } from '@/api/system/group'
 
-export default defineComponent({
-  name: 'SystemGroupEdit',
-  emits: [REFRESH_DATA_EVENT],
-  setup(props, { emit }) {
-    const stateOption = {
-      idField: groupFields.idField,
-      treeFields: groupTreeFields,
-      getApi: groupGetOne,
-      listApi: groupList,
-      postApi: groupPostOne,
-      putApi: groupPutOne,
-      resetFormData: {
-        id: '',
-        parentId: '',
-        sort: 0,
-        name: ''
-      },
-      formRule: {
-        name: [{ required: true, message: '用户组名称不能为空', trigger: 'blur' }]
-      }
-    }
+const emit = defineEmits([REFRESH_DATA_EVENT])
 
-    const { mixRefs, mixState, mixMethods } = useTreeEdit(stateOption, emit)
-
-    return {
-      ...mixRefs,
-      ...toRefs(mixState),
-      ...mixMethods
-    }
+const stateOption = {
+  idField: groupFields.idField,
+  treeFields: groupTreeFields,
+  getApi: groupGetOne,
+  listApi: groupList,
+  postApi: groupPostOne,
+  putApi: groupPutOne,
+  resetFormData: {
+    id: '',
+    parentId: '',
+    sort: 0,
+    name: ''
+  },
+  formRule: {
+    parentId: [{ required: true, message: '上级用户组ID不能为空', trigger: 'blur' }],
+    name: [{ required: true, message: '用户组名称不能为空', trigger: 'blur' }]
   }
+}
+
+const { mixRefs, mixState: state, mixMethods } = useTreeEdit(stateOption, emit)
+const { form } = mixRefs
+const { open, submit, hideDialog } = mixMethods
+
+defineExpose({
+  open
 })
 </script>
--- src/views/modules/system/group/index.vue
+++ src/views/modules/system/group/index.vue
@@ -1,37 +1,98 @@
 <template>
-  <div ref="moduleRoot" :style="pageMinHeight" class="page-group fd-page">
+  <div ref="moduleRoot" :style="pageMinHeight" class="page-system-group fd-page">
     <fd-page-header v-show="showPageHeader"></fd-page-header>
     <div class="fd-page__form">
       <div class="fd-page__action">
-        <el-button v-show="hasAuth('system:group:delete')" v-waves :disabled="selectedNodes.length <= 0" plain size="medium" type="danger" @click="del()">
+        <el-button
+          v-show="hasAuth('system:group:delete')"
+          v-waves
+          :disabled="state.selectedNodes.length <= 0"
+          plain
+          size="medium"
+          type="danger"
+          @click="del()"
+        >
           <fd-icon class="is-in-btn" icon="delete"></fd-icon>
           批量删除
         </el-button>
         <div class="action-right">
-          <el-button v-show="hasAuth('system:group:add')" v-waves plain size="medium" type="primary" @click="showEdit()">新增</el-button>
-          <el-button v-show="hasAuth('system:group:export')" v-waves size="medium" @click="exportData()">导出数据</el-button>
+          <el-button
+            v-show="hasAuth('system:group:add')"
+            v-waves
+            plain
+            size="medium"
+            type="primary"
+            @click="showEdit()"
+          >
+            新增
+          </el-button>
+          <el-button v-show="hasAuth('system:group:export')" v-waves plain size="medium" @click="exportData()">
+            导出数据
+          </el-button>
         </div>
       </div>
     </div>
     <div class="fd-page__table is-bordered">
-      <el-table ref="table" v-loading="loading" :data="data" :default-expand-all="true" :indent="15" row-key="id" style="width: 100%" @select="onSelect" @select-all="onSelectAll">
-        <el-table-column align="center" header-align="center" type="selection" width="40"></el-table-column>
-        <el-table-column :show-overflow-tooltip="true" align="center" header-align="center" label="用户组名称" prop="name"></el-table-column>
-        <el-table-column :show-overflow-tooltip="true" align="center" header-align="center" label="上级用户组" prop="parentId">
+      <el-table
+        ref="table"
+        v-loading="state.loading"
+        :data="state.data"
+        :default-expand-all="true"
+        :indent="15"
+        row-key="id"
+        style="width: 100%"
+        @select="onSelect"
+        @select-all="onSelectAll"
+      >
+        <el-table-column align="left" header-align="left" type="selection" width="40"></el-table-column>
+        <el-table-column
+          :show-overflow-tooltip="true"
+          align="left"
+          header-align="left"
+          label="用户组名称"
+          prop="name"
+        ></el-table-column>
+        <el-table-column
+          :show-overflow-tooltip="true"
+          align="left"
+          header-align="left"
+          label="上级用户组"
+          prop="parentId"
+        >
           <template #default="scope">
             {{ getParentName(scope.row.parentId) }}
           </template>
         </el-table-column>
-        <el-table-column :show-overflow-tooltip="true" align="center" header-align="center" label="排序" prop="sort"></el-table-column>
-        <el-table-column align="center" fixed="right" header-align="center" label="操作" width="100">
+        <el-table-column
+          :show-overflow-tooltip="true"
+          align="left"
+          header-align="left"
+          label="排序"
+          prop="sort"
+        ></el-table-column>
+        <el-table-column align="left" fixed="right" header-align="left" label="操作" width="100">
           <template #default="scope">
-            <el-tooltip :show-after="500" content="编辑" placement="top">
-              <el-button v-show="hasAuth('system:group:edit')" class="fd-tb-act" plain size="mini" type="success" @click="showEdit(scope.row.id)">
+            <el-tooltip content="编辑" placement="top" :show-after="500">
+              <el-button
+                v-show="hasAuth('system:group:edit')"
+                class="fd-tb-act"
+                plain
+                size="mini"
+                type="success"
+                @click="showEdit(scope.row.id)"
+              >
                 <fd-icon icon="write"></fd-icon>
               </el-button>
             </el-tooltip>
-            <el-tooltip :show-after="500" content="删除" placement="top">
-              <el-button v-show="hasAuth('system:group:delete')" class="fd-tb-act" plain size="mini" type="danger" @click="del(scope.row, scope.row.k)">
+            <el-tooltip content="删除" placement="top" :show-after="500">
+              <el-button
+                v-show="hasAuth('system:group:delete')"
+                class="fd-tb-act"
+                plain
+                size="mini"
+                type="danger"
+                @click="del(scope.row, scope.row.k)"
+              >
                 <fd-icon icon="close"></fd-icon>
               </el-button>
             </el-tooltip>
@@ -40,45 +101,37 @@
       </el-table>
     </div>
     <el-backtop></el-backtop>
-    <edit v-if="editShow" ref="editDialog" @refresh-data-list="getList"></edit>
+    <edit v-if="state.editShow" ref="editDialog" @refresh-data-list="getList"></edit>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, toRefs } from 'vue'
+export default {
+  name: 'SystemGroup'
+}
+</script>
+
+<script setup lang="ts">
 import useTree from '@/components/crud/use-tree'
-import { groupDel, groupExport, groupFields, groupList, groupTreeFields, IGroup } from '@/api/system/group'
+import { groupFields, groupTreeFields, groupList, groupDel, groupExport, IGroup } from '@/api/system/group'
 import Edit from './edit.vue'
-import useExpandTransition from '@/components/transition/use-expand-transition'
 import { getTreeNode } from '@/utils/data-tree'
 
-export default defineComponent({
-  name: 'SystemGroup',
-  components: { Edit },
-  setup() {
-    const stateOption = {
-      idField: groupFields.idField,
-      treeFields: groupTreeFields,
-      listApi: groupList,
-      delApi: groupDel,
-      exportApi: groupExport
-    }
+const stateOption = {
+  idField: groupFields.idField,
+  treeFields: groupTreeFields,
+  listApi: groupList,
+  delApi: groupDel,
+  exportApi: groupExport
+}
 
-    const { mixRefs, mixState, mixComputed, mixMethods } = useTree(stateOption)
+const { mixRefs, mixState: state, mixComputed, mixMethods } = useTree(stateOption)
+const { editDialog } = mixRefs
+const { pageMinHeight, showPageHeader } = mixComputed
+const { showEdit, getList, del, onSelect, onSelectAll, hasAuth, exportData } = mixMethods
 
-    const getParentName = (parentId: string) => {
-      const parent = getTreeNode(mixState.data, (n) => n.id === parentId)
-      return parent ? (parent as IGroup).name : '无'
-    }
-
-    return {
-      ...mixRefs,
-      ...toRefs(mixState),
-      ...mixComputed,
-      ...mixMethods,
-      ...useExpandTransition(),
-      getParentName
-    }
-  }
-})
+const getParentName = (parentId: string) => {
+  const parent = getTreeNode(state.data, (n) => n.id === parentId)
+  return parent ? (parent as IGroup).name : '无'
+}
 </script>
