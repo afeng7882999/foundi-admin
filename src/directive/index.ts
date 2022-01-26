1dc9bbe1570526f54f51f73342127d8ce729b4e5
afeng7882999 <afeng7882999@163.com>
2022-01-27 01:18:16 +0800

    feat(directive/drag-dialog): remove

--- src/directive/index.ts
+++ src/directive/index.ts
@@ -1,8 +1,6 @@
 import WavesDirective from './waves'
-import DragDialogDirective from './drag-dialog'
 import { App } from 'vue'
 
 export default function installDirective(app: App): void {
   app.directive('waves', WavesDirective)
-  app.directive('drag-dialog', DragDialogDirective)
 }
