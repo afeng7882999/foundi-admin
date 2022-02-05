b281daf70c14cfddd1b385a919bbf49c6373a892
afeng7882999 <afeng7882999@163.com>
2022-02-06 00:14:08 +0800

    refactor(component/table): move shared 'visible' condition to outer template

--- src/components/table/column.vue
+++ src/components/table/column.vue
@@ -1,127 +1,129 @@
 <template>
-  <!-- default -->
-  <el-table-column
-    v-if="typ === 'default' && visible"
-    v-bind="$attrs"
-    :show-overflow-tooltip="showOverflowTooltip"
-    :align="align"
-    :header-align="headerAlign"
-    :width="width"
-    :label-class-name="sortable ? 'is-custom' : ''"
-  >
-    <template v-if="sortable" #header="scope">
-      <fd-table-sort-header :column="scope.column" @sort-changed="onSortChanged"></fd-table-sort-header>
-    </template>
-  </el-table-column>
-  <!-- custom -->
-  <el-table-column
-    v-if="typ === 'custom' && visible"
-    v-bind="$attrs"
-    :show-overflow-tooltip="showOverflowTooltip"
-    :align="align"
-    :header-align="headerAlign"
-    :width="width"
-    :label-class-name="sortable ? 'is-custom' : ''"
-  >
-    <template #default="scope">
-      <slot :row="scope.row" :idx="scope.$index"></slot>
-    </template>
-    <template v-if="sortable" #header="scope">
-      <fd-table-sort-header :column="scope.column" @sort-changed="onSortChanged"></fd-table-sort-header>
-    </template>
-  </el-table-column>
-  <!-- selection -->
-  <el-table-column
-    v-else-if="typ === 'selection' && visible"
-    v-bind="$attrs"
-    :align="align"
-    :header-align="headerAlign"
-    type="selection"
-    width="40"
-  ></el-table-column>
-  <!-- datetime -->
-  <el-table-column
-    v-else-if="typ === 'datetime' && visible"
-    v-bind="$attrs"
-    :align="align"
-    :header-align="headerAlign"
-    :width="widthCo"
-    :label-class-name="sortable ? 'is-custom' : ''"
-  >
-    <template #default="scope">
-      <span>{{ formatTimestamp(scope.row.operTime) }}</span>
-    </template>
-    <template v-if="sortable" #header="scope">
-      <fd-table-sort-header :column="scope.column" @sort-changed="onSortChanged"></fd-table-sort-header>
-    </template>
-  </el-table-column>
-  <!-- dictionary -->
-  <el-table-column
-    v-else-if="typ === 'dict' && visible"
-    v-bind="$attrs"
-    :align="align"
-    :header-align="headerAlign"
-    :width="widthCo"
-    :label-class-name="sortable ? 'is-custom' : ''"
-  >
-    <template #default="scope">
-      <span>{{ dictVal(dict, scope.row[attrs.prop]) }}</span>
-    </template>
-    <template v-if="sortable" #header="scope">
-      <fd-table-sort-header :column="scope.column" @sort-changed="onSortChanged"></fd-table-sort-header>
-    </template>
-  </el-table-column>
-  <!-- action -->
-  <el-table-column
-    v-else-if="typ === 'act'"
-    v-bind="$attrs"
-    :align="align"
-    fixed="right"
-    :header-align="headerAlign"
-    label="操作"
-    :width="widthCo"
-  >
-    <template #default="scope">
-      <slot name="prefix" :row="scope.row" :idx="scope.$index"></slot>
-      <el-tooltip :show-after="500" content="详细" placement="top">
-        <el-button
-          v-show="detailVisible"
-          class="fd-tb-act fd-tb-act-detail"
-          plain
-          size="mini"
-          type="primary"
-          @click="actionEmit('detail', scope.row, scope.$index)"
-        >
-          <fd-icon icon="view-grid-detail"></fd-icon>
-        </el-button>
-      </el-tooltip>
-      <el-tooltip :show-after="500" content="编辑" placement="top">
-        <el-button
-          v-show="editVisible"
-          class="fd-tb-act"
-          plain
-          size="mini"
-          type="success"
-          @click="actionEmit('edit', scope.row, scope.$index)"
-        >
-          <fd-icon icon="write"></fd-icon>
-        </el-button>
-      </el-tooltip>
-      <el-tooltip :show-after="500" content="删除" placement="top">
-        <el-button
-          v-show="delVisible"
-          class="fd-tb-act"
-          plain
-          size="mini"
-          type="danger"
-          @click="actionEmit('del', scope.row, scope.$index)"
-        >
-          <fd-icon icon="close"></fd-icon>
-        </el-button>
-      </el-tooltip>
-      <slot name="append" :row="scope.row" :idx="scope.$index"></slot>
-    </template>
-  </el-table-column>
+  <template v-if="visible">
+    <!-- default -->
+    <el-table-column
+      v-if="typ === 'default'"
+      v-bind="$attrs"
+      :show-overflow-tooltip="showOverflowTooltip"
+      :align="align"
+      :header-align="headerAlign"
+      :width="width"
+      :label-class-name="sortable ? 'is-custom' : ''"
+    >
+      <template v-if="sortable" #header="scope">
+        <fd-table-sort-header :column="scope.column" @sort-changed="onSortChanged"></fd-table-sort-header>
+      </template>
+    </el-table-column>
+    <!-- custom -->
+    <el-table-column
+      v-if="typ === 'custom'"
+      v-bind="$attrs"
+      :show-overflow-tooltip="showOverflowTooltip"
+      :align="align"
+      :header-align="headerAlign"
+      :width="width"
+      :label-class-name="sortable ? 'is-custom' : ''"
+    >
+      <template #default="scope">
+        <slot :row="scope.row" :idx="scope.$index"></slot>
+      </template>
+      <template v-if="sortable" #header="scope">
+        <fd-table-sort-header :column="scope.column" @sort-changed="onSortChanged"></fd-table-sort-header>
+      </template>
+    </el-table-column>
+    <!-- selection -->
+    <el-table-column
+      v-else-if="typ === 'selection'"
+      v-bind="$attrs"
+      :align="align"
+      :header-align="headerAlign"
+      type="selection"
+      width="40"
+    ></el-table-column>
+    <!-- datetime -->
+    <el-table-column
+      v-else-if="typ === 'datetime'"
+      v-bind="$attrs"
+      :align="align"
+      :header-align="headerAlign"
+      :width="widthCo"
+      :label-class-name="sortable ? 'is-custom' : ''"
+    >
+      <template #default="scope">
+        <span>{{ formatTimestamp(scope.row.operTime) }}</span>
+      </template>
+      <template v-if="sortable" #header="scope">
+        <fd-table-sort-header :column="scope.column" @sort-changed="onSortChanged"></fd-table-sort-header>
+      </template>
+    </el-table-column>
+    <!-- dictionary -->
+    <el-table-column
+      v-else-if="typ === 'dict'"
+      v-bind="$attrs"
+      :align="align"
+      :header-align="headerAlign"
+      :width="widthCo"
+      :label-class-name="sortable ? 'is-custom' : ''"
+    >
+      <template #default="scope">
+        <span>{{ dictVal(dict, scope.row[attrs.prop]) }}</span>
+      </template>
+      <template v-if="sortable" #header="scope">
+        <fd-table-sort-header :column="scope.column" @sort-changed="onSortChanged"></fd-table-sort-header>
+      </template>
+    </el-table-column>
+    <!-- action -->
+    <el-table-column
+      v-else-if="typ === 'act'"
+      v-bind="$attrs"
+      :align="align"
+      fixed="right"
+      :header-align="headerAlign"
+      label="操作"
+      :width="widthCo"
+    >
+      <template #default="scope">
+        <slot name="prefix" :row="scope.row" :idx="scope.$index"></slot>
+        <el-tooltip :show-after="500" content="详细" placement="top">
+          <el-button
+            v-show="detailVisible"
+            class="fd-tb-act fd-tb-act-detail"
+            plain
+            size="mini"
+            type="primary"
+            @click="actionEmit('detail', scope.row, scope.$index)"
+          >
+            <fd-icon icon="view-grid-detail"></fd-icon>
+          </el-button>
+        </el-tooltip>
+        <el-tooltip :show-after="500" content="编辑" placement="top">
+          <el-button
+            v-show="editVisible"
+            class="fd-tb-act"
+            plain
+            size="mini"
+            type="success"
+            @click="actionEmit('edit', scope.row, scope.$index)"
+          >
+            <fd-icon icon="write"></fd-icon>
+          </el-button>
+        </el-tooltip>
+        <el-tooltip :show-after="500" content="删除" placement="top">
+          <el-button
+            v-show="delVisible"
+            class="fd-tb-act"
+            plain
+            size="mini"
+            type="danger"
+            @click="actionEmit('del', scope.row, scope.$index)"
+          >
+            <fd-icon icon="close"></fd-icon>
+          </el-button>
+        </el-tooltip>
+        <slot name="append" :row="scope.row" :idx="scope.$index"></slot>
+      </template>
+    </el-table-column>
+  </template>
 </template>
 
 <script lang="ts">
