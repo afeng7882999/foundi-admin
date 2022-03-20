3d40354a9058f99ff0aa99b45c6accb83f5cea8a
afeng7882999 <afeng7882999@163.com>
2022-03-21 00:50:11 +0800

    feat: new component FdFmtList

--- src/extend/table/index.ts
+++ src/extend/table/index.ts
@@ -5,6 +5,7 @@ import FdFmtDatetime from './formatter/fmt-datetime.vue'
 import FdFmtDict from './formatter/fmt-dict.vue'
 import FdFmtIcon from './formatter/fmt-icon.vue'
 import FdFmtJson from './formatter/fmt-json.vue'
+import FdFmtList from './formatter/fmt-list.vue'
 
 import FdColAct from './components/col-act.vue'
 import FdColCustom from './components/col-custom.vue'
@@ -12,6 +13,7 @@ import FdColDatetime from './components/col-datetime.vue'
 import FdCol from './components/col.vue'
 import FdColDict from './components/col-dict.vue'
 import FdColIcon from './components/col-icon.vue'
+import FdColList from './components/col-list.vue'
 import FdColSelection from './components/col-selection.vue'
 
 export default function install(app: App) {
@@ -21,6 +23,7 @@ export default function install(app: App) {
   app.component(FdFmtDict.name, FdFmtDict)
   app.component(FdFmtIcon.name, FdFmtIcon)
   app.component(FdFmtJson.name, FdFmtJson)
+  app.component(FdFmtList.name, FdFmtList)
 
   app.component(FdColAct.name, FdColAct)
   app.component(FdColCustom.name, FdColCustom)
@@ -28,5 +31,6 @@ export default function install(app: App) {
   app.component(FdCol.name, FdCol)
   app.component(FdColDict.name, FdColDict)
   app.component(FdColIcon.name, FdColIcon)
+  app.component(FdColList.name, FdColList)
   app.component(FdColSelection.name, FdColSelection)
 }
