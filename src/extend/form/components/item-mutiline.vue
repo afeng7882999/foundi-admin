b5d6a045269fd459f22c5072d33e35e053532925
afeng7882999 <afeng7882999@163.com>
2022-03-25 00:10:15 +0800

    feat: new 'FdFmiMultiline' component

---
+++ src/extend/form/components/item-mutiline.vue
@@ -0,0 +1,33 @@
+<template>
+  <template v-if="visibleCo">
+    <el-form-item v-bind="$attrs" :label="label" :prop="prop">
+      <el-input
+        v-model="model()[prop]"
+        clearable
+        :placeholder="placeholderCo"
+        :style="styleCo"
+        :disabled="disabledCo"
+        :autosize="{ minRows: 3 }"
+        type="textarea"
+      />
+    </el-form-item>
+  </template>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'FdFmiMultiline',
+  inheritAttrs: false
+}
+</script>
+
+<script setup lang="ts">
+import { FORM_ITEM_DEFAULT_PROPS } from '@/extend/form/type'
+import useFormItem from '@/extend/form/hooks/use-form-item'
+
+const props = defineProps({
+  ...FORM_ITEM_DEFAULT_PROPS
+})
+
+const { model, placeholderCo, visibleCo, disabledCo, styleCo } = useFormItem(props)
+</script>
