9a12a9267a8acccd0b131f9b934c48f0096e6f6e
afeng7882999 <afeng7882999@163.com>
2022-03-25 00:10:05 +0800

    feat: new 'FdFmiBoolean' component

---
+++ src/extend/form/components/item-boolean.vue
@@ -0,0 +1,59 @@
+<template>
+  <template v-if="visibleCo">
+    <el-form-item v-bind="$attrs" :label="label" :prop="prop">
+      <template v-if="type === 'switch'">
+        <el-switch
+          v-model="model()[prop]"
+          clearable
+          :placeholder="placeholderCo"
+          :style="styleCo"
+          :disabled="disabledCo"
+          :active-text="labelsCo[0]"
+          :inactive-text="labelsCo[1]"
+        />
+      </template>
+      <template v-else>
+        <el-select v-model="model()[prop]" clearable :disabled="disabledCo" :placeholder="placeholderCo" :style="styleCo" @change="submit">
+          <el-option :label="labelsCo[0]" :value="true"></el-option>
+          <el-option :label="labelsCo[1]" :value="false"></el-option>
+        </el-select>
+      </template>
+    </el-form-item>
+  </template>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'FdFmiBoolean',
+  inheritAttrs: false
+}
+</script>
+
+<script setup lang="ts">
+import { FORM_ITEM_DEFAULT_PROPS } from '@/extend/form/type'
+import useFormItem from '@/extend/form/hooks/use-form-item'
+import { computed, PropType } from 'vue'
+
+const props = defineProps({
+  ...FORM_ITEM_DEFAULT_PROPS,
+  type: {
+    type: String as PropType<'switch' | 'select'>,
+    default: 'switch',
+    validator: (val: string) => ['switch', 'select'].includes(val)
+  },
+  labels: {
+    type: String,
+    default: '是,否'
+  }
+})
+
+const labelsCo = computed(() => {
+  if (props.labels) {
+    const pl = props.labels.split(',').map((s) => s.trim())
+    return [pl[0], pl[1]]
+  }
+  return ['是', '否']
+})
+
+const { model, placeholderCo, visibleCo, disabledCo, styleCo, submit } = useFormItem(props)
+</script>
