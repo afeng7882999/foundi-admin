b30375708d04a16732c5a0798401c52f0160ec27
afeng7882999 <afeng7882999@163.com>
2022-03-25 00:10:56 +0800

    refactor: remove 'FdPageModal'

--- src/extend/page/index.ts
+++ src/extend/page/index.ts
@@ -2,11 +2,9 @@ import { App } from 'vue'
 import FdPage from '@/extend/page/page.vue'
 import FdPageAct from '@/extend/page/page-act.vue'
 import FdPageFooter from '@/extend/page/page-footer.vue'
-import FdPageModal from '@/extend/page/page-modal.vue'
 
 export default function install(app: App) {
   app.component(FdPage.name, FdPage)
   app.component(FdPageAct.name, FdPageAct)
   app.component(FdPageFooter.name, FdPageFooter)
-  app.component(FdPageModal.name, FdPageModal)
 }
