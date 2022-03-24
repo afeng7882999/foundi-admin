51dc3cd0c4de98703a77956df65649eb0c3825ce
afeng7882999 <afeng7882999@163.com>
2022-03-25 00:10:45 +0800

    refactor: rename 'FdFmiDatetime' to 'FdFmiDateRange'

--- src/extend/form/index.ts
+++ src/extend/form/index.ts
@@ -1,18 +1,22 @@
 import { App } from 'vue'
-import FdFmiDatetime from './components/item-datetime.vue'
+import FdFmiDateRange from './components/item-date-range.vue'
 import FdFmiDict from './components/item-dict.vue'
 import FdFmi from './components/item.vue'
 import FdFmiList from './components/item-list.vue'
 import FdFmiTree from './components/item-tree.vue'
 import FdFmiSort from './components/item-sort.vue'
 import FdFmiJson from './components/item-json.vue'
+import FdFmiMultiline from './components/item-mutiline.vue'
+import FdFmiBoolean from './components/item-boolean.vue'
 
 export default function install(app: App) {
-  app.component(FdFmiDatetime.name, FdFmiDatetime)
+  app.component(FdFmiDateRange.name, FdFmiDateRange)
   app.component(FdFmiDict.name, FdFmiDict)
   app.component(FdFmi.name, FdFmi)
   app.component(FdFmiList.name, FdFmiList)
   app.component(FdFmiTree.name, FdFmiTree)
   app.component(FdFmiSort.name, FdFmiSort)
   app.component(FdFmiJson.name, FdFmiJson)
+  app.component(FdFmiMultiline.name, FdFmiMultiline)
+  app.component(FdFmiBoolean.name, FdFmiBoolean)
 }
