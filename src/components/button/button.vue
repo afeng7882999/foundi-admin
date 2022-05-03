2bd444824aadeab10d3eafbe39c4a770a289640f
afeng7882999 <afeng7882999@163.com>
2022-05-04 00:02:24 +0800

    fix: remove import 'defineProps'

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineProps, PropType } from 'vue'
+import { computed, PropType } from 'vue'
 import { buttonColors, buttonColorsType, buttonTypes, buttonTypesType } from './types'
 
 defineOptions({
